5414c170f324dd7123d4424ca7aa9719f342292f
Elad Alon <elad.alon@gmail.com>
2022-08-24 14:53:16 +0200

    Seed index.html

---
+++ multicapture.js
@@ -0,0 +1,23 @@
+var respecConfig = {
+  group: "webrtc",
+  specStatus: "unofficial",
+  github: {
+    repoURL: "https://github.com/wicg/multicapture/",
+    branch: "master",
+  },
+  editors: [
+    {
+      name: "Simon Hangl",
+      email: "simonha@google.com",
+      company: "Google",
+    },
+    {
+      name: "Elad Alon",
+      email: "eladalon@google.com",
+      company: "Google",
+    },
+  ],
+  xref: ["html", "infra", "permissions", "permissions-policy", "dom", "mediacapture-streams", "webidl", "screen-capture"],
+  shortName: "multicapture",
+  subjectPrefix: "[multicapture]",
+};
